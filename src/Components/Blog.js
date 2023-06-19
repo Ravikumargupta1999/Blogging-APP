523bdf8efe76005d3c5586515658cde53044821e
unknown <ravikumargupta391999@gmail.com>
2023-06-19 19:29:27 +0530

    Setting Up Initial State

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,45 +1,56 @@
-export default function Blog(){
+import { useState } from "react";
 
-    function handleSubmit(e){
+export default function Blog() {
+
+
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    function handleSubmit(e) {
         e.preventDefault();
     }
 
-    return(
+    return (
         <>
-        <h1>Blogging APP</h1>
-        <div className="section">
-            <form onSubmit={handleSubmit}>
-                <Row label="Title">
+            <h1>Blogging APP</h1>
+            <div className="section">
+                <form onSubmit={handleSubmit}>
+                    <Row label="Title">
                         <input className="input"
-                                placeholder="Enter the Title of the Blog here.."/>
-                </Row >
-                <Row label="Content">
-                        <textarea className="input content"
-                                placeholder="Content of the Blog goes here.."
+                            placeholder="Enter the Title of the Blog here.." 
+                            value={title} 
+                            onChange={(e) =>{setTitle(e.target.value)}}
                         />
-                </Row >
+                    </Row >
+                    <Row label="Content">
+                        <textarea className="input content"
+                            placeholder="Content of the Blog goes here.."
+                            value={content}
+                            onChange={(e) =>{setContent(e.target.value)}}
+                        />
+                    </Row >
 
+                    <button className="btn">ADD</button>
+                </form>
 
-                <button className = "btn">ADD</button>
-            </form>
-                     
-        </div>
+            </div>
 
-        <hr/>
+            <hr />
 
-        <h2> Blogs </h2>
-        
+            <h2> Blogs </h2>
+            <h3>{title}</h3>
+            <p>{content}</p>
         </>
-        )
-    }
+    )
+}
 
-function Row(props){
-    const{label} = props;
-    return(
+function Row(props) {
+    const { label } = props;
+    return (
         <>
-        <label>{label}<br/></label>
-        {props.children}
-        <hr />
+            <label>{label}<br /></label>
+            {props.children}
+            <hr />
         </>
     )
 }
